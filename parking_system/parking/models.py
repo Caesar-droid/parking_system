988ceeb46288f65db722fc712e58cda7c7c8f328
Caesar-droid <dominictoroitichh@gmail.com>
2021-11-17 23:13:59 +0300

    adding models db

--- parking_system/parking/models.py
+++ parking_system/parking/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    first_name=models.CharField(max_length=30)
+    second_name=models.CharField(max_length=30)
+    email=models.EmailField(max_length=250)
+    phone=models.CharField(max_length=50)
+class Vehicle(models.Model):
+    CAR_TYPES=[
+        ('S','SALOON'),
+        ('T','HEAVY DUTY'),
+        ('X','SUV'),
+    ]
+    driver=models.ForeignKey(Person, on_delete=models.CASCADE)
+    car_type=models.CharField(max_length=1,choices=CAR_TYPES)
+    car_plate=models.CharField(max_length=15)
+class ParkingLot(models.Model):
+    vehicle=models.OneToOneField(Vehicle,null=True,blank=True,on_delete=models.SET_NULL)
+    available=models.BooleanField(default=False)
+    start_time=models.DateField()
+    end_time=models.DateField()
+    paid_amount=models.IntegerField()
