4b6b14b7de9997e8084aa7f93bed5a55000aabe3
Caesar-droid <dominictoroitichh@gmail.com>
2021-12-01 20:34:00 +0300

    adding register view

--- parking_system/parking/views.py
+++ parking_system/parking/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from .form import PersonForm,VehicleForm,ParkingLotForm
 
 def home(request):
     return render(request,'parking/home.html')
+class RegisterView(View):
+    def get(self,request,*args,**kwargs):
+        return render(request,'parking/register.html',{'person_form':PersonForm,'vehicle_form':VehicleForm,'parking_form':ParkingLotForm})
 
